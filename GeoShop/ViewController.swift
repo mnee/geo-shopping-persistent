a3712f1e93e064e84fd41bcd810c4426ecb25db9
Mikhail Nee <addisonorangeboy@gmail.com>
2017-11-30 21:29:09 -0800

    Initial Commit

---
+++ GeoShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoShop
+//
+//  Created by Mischa Nee on 11/30/17.
+//  Copyright © 2017 Mischa Nee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
